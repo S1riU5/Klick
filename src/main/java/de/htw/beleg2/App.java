65fc0b317728ab6d4f57aef6c2b526885957e727
sebsch <s0543196@htw-berlin.de>
2013-12-18 16:44:58 +0100

    new mavenish structure

---
+++ src/main/java/de/htw/beleg2/App.java
@@ -0,0 +1,39 @@
+package de.htw.beleg2;
+
+
+
+/* what is needed?
+ * 
+ * [ ] Gamearea-Array n = 2
+ * 
+ * [ ] several fields
+ * 
+ * [ ] check the neighbors
+ * 
+ * [ ] close the holes
+ * 
+ * [ ] UIpackage klickibunti;
+
+ * 
+ * [ ] GUI
+ * 
+ */
+
+
+
+
+public class App {
+	/**
+	 * @author s0543196 
+	 * @author s0537090
+	 * 
+	 * @param args
+	 * 
+	 */
+
+
+	public static void main(String[] args) {
+		cliUI ui = new cliUI();
+	}
+}
+
